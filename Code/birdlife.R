dfbd2f82db8085e3bbe061b1606e90582abab50f
friederikegebert <58672382+friederikegebert@users.noreply.github.com>
2025-07-01 11:07:18 +0200

    Update birdlife.R

--- Code/birdlife.R
+++ Code/birdlife.R
@@ -1,4 +1,10 @@
+
+library(terra)
 library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(rgbif)
+library(tidyverse)
 
 birds <- "BOTW_2024_2.gpkg"
 data <- st_read(birds)
@@ -15,16 +21,26 @@ st_layers(birds)
 birds_sf <- st_read(birds, layer ="all_species")
 
 st_write(birds_sf, "birds_allsp.gpkg", layer = "all_species")
-
+birds_sf <- st_read("birds_allsp.gpkg", layer = "all_species")
 
 str(birds_sf)
 names(birds_sf)
 head(birds_sf)
 unique(birds_sf$sci_name)
 
-plot(birds_sf["sci_name"])
+unique(birds_sf$presence)
+# plot(birds_sf["sci_name"])
 
-crop_to_germany <- function(data){ 
+# filter to native, extant, resident 
+
+birds_subset <- birds_sf %>% 
+  filter(origin ==1, presence ==1, seasonal==1)
+
+# cast geometry
+birds_sf_fixed <- st_cast(birds_sf, "MULTIPOLYGON", warn = FALSE)
+
+
+crop_to_germany_raster <- function(data){ 
 germany_sf <- ne_countries(country = "Germany", scale = "medium", returnclass = "sf")
 germany_sf <- st_transform(germany_sf, crs = crs(data))
 germany_vect <- vect(germany_sf)
@@ -33,3 +49,26 @@ germ <- mask(r_crop, germany_vect)
 plot(germ)
 return(germ)
 }
+
+crop_to_germany_vector <- function(data){ 
+  germany_sf <- ne_countries(country = "Germany", scale = "medium", returnclass = "sf")
+  germany_sf <- st_transform(germany_sf, crs = crs(data))
+  germ <- st_intersection(data, germany_sf)
+  plot(st_geometry(germ))
+  return(germ)
+}
+
+crop_to_germany_vector(birds_sf_fixed)
+
+
+
+# Check which geometries are invalid
+invalid_idx <- which(!st_is_valid(birds_sf_fixed))
+
+length(invalid_idx)  # How many?
+
+# Option 1: Make geometries valid
+birds_sf_fixed$geometry <- st_make_valid(birds_sf_fixed$geometry)
+
+# Option 2: Filter out invalid geometries (if making valid fails)
+birds_sf_valid <- birds_sf_fixed[st_is_valid(birds_sf_fixed), ]
